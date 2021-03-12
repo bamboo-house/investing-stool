185950653a63f26d7ed4749cd3939e5ac6bc75e6
Takeuchi Shuto <takebamboo001@icloud.com>
2021-03-12 16:22:41 +0900

    update2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # get 'home/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "Stool/home" => "home#top"
-  get "Stool/home/stock_per" => "home#stock_per"
-  post "Stool/home/stock_per" => "home#stock_per"
+  get "/" => "home#top"
+  get "stock_per" => "home#stock_per"
+  post "stock_per" => "home#stock_per"
 end
