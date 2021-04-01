e9041e110d000f7f013da5e098f793e86be87a9a
Takeuchi Shuto <takebamboo001@icloud.com>
2021-04-02 02:08:11 +0900

    cupdate roe pbr page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,15 +16,27 @@ class HomeController < ApplicationController
   end
 
   def stock_pbr
+    stock_price = params[:stock_price].to_f
+    bps = params[:bps].to_f
 
+    market_cap = params[:market_cap].to_f
+    net_assets = params[:net_assets].to_f
 
-
+    @result_pbr1 = (stock_price / bps).round(2)
+    @result_pbr2 = (market_cap / net_assets).round(2)
     
     @current_index = "PBR"
   end
 
   def stock_roe
-       
+    net_income = params[:net_income].to_f
+    net_assets= params[:net_assets].to_f
+
+    eps = params[:eps].to_f
+    bps = params[:bps].to_f
+
+    @result_roe1 = (net_income / net_assets).round(4) * 100 
+    @result_roe2 = (eps / bps).round(4) * 100 
 
 
     
